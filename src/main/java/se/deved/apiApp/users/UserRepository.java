20bba93955d9a5a7102181747ecd06c7ab990688
Olga2992930 <olga2992930@gmail.com>
2025-03-07 20:25:44 +0100

    SecurityConfig

--- src/main/java/se/deved/apiApp/users/UserRepository.java
+++ src/main/java/se/deved/apiApp/users/UserRepository.java
@@ -1,4 +1,16 @@
 package se.deved.apiApp.users;
 
-public class UserRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserEntity, UUID> {
+    Optional<UserEntity> findByName(String name);
+    Optional<UserEntity> findByOidcId(String id);
 }
+
+
+
--- src/main/java/se/deved/apiApp/users/UserService.java
+++ src/main/java/se/deved/apiApp/users/UserService.java
@@ -1,4 +1,19 @@
 package se.deved.apiApp.users;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.Optional;
+import se.deved.apiApp.users.UserEntity;
+import se.deved.apiApp.users.UserRepository;
+
+@Service
 public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+    public Optional<UserEntity> findByUsername(String username) {
+        return userRepository.findByUsername(username);
+    }
 }
+
+
+
